eef440c760668abf5992394f5b726cd9f81e7dc4
mythnetgames <mythnetgames@gmail.com>
2026-02-26 10:19:56 -0600

    Add holiday description to time display

--- src/act.informative.cpp
+++ src/act.informative.cpp
@@ -7939,9 +7939,10 @@ time_string(CHAR_DATA * ch) {
 
 	/* Holiday annotation */
 	if (time_info.holiday >= 0) {
-		sprintf(buf + strlen(buf), "\n   #2%s#0 (%s).",
+		sprintf(buf + strlen(buf), "\n   #2%s#0 (%s).\n   %s",
 				holiday_table[time_info.holiday].name,
-				patron_name[holiday_table[time_info.holiday].patron]);
+				patron_name[holiday_table[time_info.holiday].patron],
+				holiday_table[time_info.holiday].description);
 	}
 
 	sprintf(buf + strlen(buf), " %s, year %d AH.\n",
